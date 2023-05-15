25d41fdb7ee102a7ac130ee939b2bcb4ccf2f305
bucknorris336 <buckblocker90gmail.com>
2023-05-15 00:30:52 -0400

    added to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,42 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <footer className="footer">
-      <ul>
-        <li>
-          <a
-            href="https://github.com/bucknorris336"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-github"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/buck-blocker-802a9124b"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-linkedin"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="mailto:buckblocker90@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fas fa-envelope"></i>
-          </a>
-        </li>
-      </ul>
+    <footer className="footer bg-dark">
+      <Container>
+        <Row>
+          <Col className="text-center">
+            <a
+              href="https://github.com/bucknorris336"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-icon"
+            >
+              <FaGithub size={24} color="white" className="hover-link" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/buck-blocker-802a9124b"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-icon"
+            >
+              <FaLinkedin size={24} color="white" className="hover-link" />
+            </a>
+            <a
+              href="mailto:buckblocker90@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-icon"
+            >
+              <FaEnvelope size={24} color="white" className="hover-link" />
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
-}
+};
+
+export default Footer;
