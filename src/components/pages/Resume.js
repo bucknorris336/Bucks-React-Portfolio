f68490ad4a7963f77354507da76d00e0acd3f22a
bucknorris336 <buckblocker90gmail.com>
2023-05-15 00:12:06 -0400

    added more to resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,41 @@
 import React from "react";
-export default function Resume() {
+import { Container, ListGroup, Button } from "react-bootstrap";
+import resumePdf from "../assets/resume.pdf";
+
+const Resume = () => {
+  const proficiencies = [
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Node.js",
+    // Add more proficiencies as needed
+  ];
+
   return (
-    <div className="div-container">
-      <h1>Resume Page</h1>
-      <p>
-        I possess a diverse skill set in full-stack development that spans from
-        UI/UX design to database configuration and server-side API engineering.
-        My proficiency in web development encompasses a range of technologies,
-        including React, Node.js, MySQL and Sequelize, Mongoose and MongoDB,
-        Express.js, JQuery, and CSS frameworks like Bootstrap.
-      </p>
-    </div>
+    <Container className="text-center" style={{ maxWidth: "400px" }}>
+      <h2 className="my-5">Resume</h2>
+      <ListGroup>
+        {proficiencies.map((proficiency, index) => (
+          <ListGroup.Item key={index}>{proficiency}</ListGroup.Item>
+        ))}
+      </ListGroup>
+      <Button variant="primary" href={resumePdf} download className="mt-5">
+        Download Resume
+      </Button>
+    </Container>
   );
-}
+};
+
+export default Resume;
+
+// <div className="div-container">
+//   <h1>Resume Page</h1>
+//   <p>
+//     I possess a diverse skill set in full-stack development that spans from
+//     UI/UX design to database configuration and server-side API engineering.
+//     My proficiency in web development encompasses a range of technologies,
+//     including React, Node.js, MySQL and Sequelize, Mongoose and MongoDB,
+//     Express.js, JQuery, and CSS frameworks like Bootstrap.
+//   </p>
+// </div>
