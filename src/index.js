9ddc9097760018c043c62226ea7eb32b4774415b
bucknorris336 <buckblocker90gmail.com>
2023-05-15 00:25:43 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css";
-// 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
