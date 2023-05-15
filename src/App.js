ae085dc38bdfaf16117a58683cc3eeed681e1b43
bucknorris336 <buckblocker90gmail.com>
2023-05-15 00:22:58 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
-const App = () => <PortfolioContainer />;
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <main>
+          <Container className="mb-5 mx-3">
+            <Routes>
+              <Route exact="true" path="/AboutMe" element={<AboutMe />} />
+              <Route exact="true" path="/Portfolio" element={<Portfolio />} />
+              <Route exact="true" path="/Contact" element={<Contact />} />
+              <Route exact="true" path="/Resume" element={<Resume />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
 
 export default App;
