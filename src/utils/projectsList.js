247cf71b712950e32654fd6d3b1b85b1fde6c35c
bucknorris336 <buckblocker90gmail.com>
2023-05-17 21:12:15 -0400

    updated App.css

--- src/utils/projectsList.js
+++ src/utils/projectsList.js
@@ -1,7 +1,7 @@
 export const projectsList = [
   {
-    name: "Calm Calendar",
-    imageLink:
+    title: "Calm Calendar",
+    imgSrc:
       "https://user-images.githubusercontent.com/115678318/226114806-1c2efaaa-286c-4373-a18b-05fed1776ae1.png",
     desc: "This calendar allows new and returning users to add and view events as a calendar layout. Events are stored using cookies, so when user logs back in, they can still see any past or future events.",
     techUsed: [
@@ -17,8 +17,8 @@ export const projectsList = [
     liveSite: "https://calm-calendar-app.herokuapp.com/",
   },
   {
-    name: "Pantry",
-    imageLink:
+    title: "Pantry",
+    imgSrc:
       "https://github.com/bucknorris336/project-awesome/raw/main/assets/images/screenshot.png",
     desc: "Pantry stores a list of ingredients that you currently have in your pantry at home along with new items added by you, in you shopping list.",
     techUsed: [
@@ -34,8 +34,8 @@ export const projectsList = [
     liveSite: "https://andykb9b13.github.io/project-awesome",
   },
   {
-    name: "Tech Blog",
-    imageLink:
+    title: "Tech Blog",
+    imgSrc:
       "https://user-images.githubusercontent.com/62455130/237505854-5d23aaf8-58ae-4aaa-9c22-08fccb613131.png",
     desc: "Users can view and create blog posts, interact with blog posts by commenting, and update or delete blog posts",
     techUsed: [
@@ -50,21 +50,24 @@ export const projectsList = [
     liveSite: "https://github.com/bucknorris336/Bucks-Tech-Blog",
   },
   {
-    name: "Weather Dashboard",
-    imageLink:
+    title: "Weather Dashboard",
+    imgSrc:
       "https://user-images.githubusercontent.com/62455130/237506663-9aee62b8-042b-4fc5-acc7-771fe5a9a1ce.png",
     desc: "An app that allows you to find the current weather in whatever city you'd like. It will even show the forecast for the next five days.",
     techUsed: [
-      "html",
-      "css",
-      "javaScript",
+      "express",
+      "mysql2",
+      "sequelize",
+      "connect-session-sequelize",
+      "dotenv",
+      "bcrypt",
     ],
     githubLink: "https://github.com/bucknorris336/Bucks-weather-bashboard",
     liveSite: "https://github.com/bucknorris336/Bucks-weather-bashboard",
   },
   {
-    name: "Note Taker",
-    imageLink:
+    title: "Note Taker",
+    imgSrc:
       "https://github.com/bucknorris336/bucks-note-taker/raw/main/assets/salty-gorge-1.png",
     desc: "This application uses an express backend, saving and retrieving JSON stored data. It can be used to write and save notes.",
     techUsed: ["Heroku deployment", "express.js", "back-end db routes"],
@@ -72,8 +75,8 @@ export const projectsList = [
     liveSite: "https://salty-gorge-36612.herokuapp.com/",
   },
   {
-    name: "Bucks Portfolio",
-    imageLink:
+    title: "Bucks Portfolio",
+    imgSrc:
       "https://user-images.githubusercontent.com/62455130/238062028-e8f48780-37a0-47b4-b30c-65ed8b42932e.png",
     desc: "A simple spash-page using html and css to practice using the languages.",
     techUsed: ["html", "css"],
