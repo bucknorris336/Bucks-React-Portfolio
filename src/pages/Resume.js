67826beac535e92e91d08568fa43296b8fbe7e46
bucknorris336 <buckblocker90gmail.com>
2023-05-18 20:40:44 -0400

    changed contact form, css, cleaned up code.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -22,13 +22,18 @@ const Resume = () => {
 
   return (
     <Container className="text-center" style={{ maxWidth: "400px" }}>
-      <h4 className="my-5">Proficiencies:</h4>
+      <h1 className="my-5">Proficiencies:</h1>
       <ListGroup>
         {proficiencies.map((proficiency, index) => (
           <ListGroup.Item key={index}>{proficiency}</ListGroup.Item>
         ))}
       </ListGroup>
-      <Button variant="primary" href={resumePdf} download className="mt-5">
+      <Button
+        variant="outline-primary"
+        href={resumePdf}
+        download
+        className="mt-5"
+      >
         Download Resume
       </Button>
     </Container>
@@ -36,14 +41,3 @@ const Resume = () => {
 };
 
 export default Resume;
-
-// <div className="div-container">
-//   <h1>Resume Page</h1>
-//   <p>
-//     I possess a diverse skill set in full-stack development that spans from
-//     UI/UX design to database configuration and server-side API engineering.
-//     My proficiency in web development encompasses a range of technologies,
-//     including React, Node.js, MySQL and Sequelize, Mongoose and MongoDB,
-//     Express.js, JQuery, and CSS frameworks like Bootstrap.
-//   </p>
-// </div>
